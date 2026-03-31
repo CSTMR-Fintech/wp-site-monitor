23056cddb37593b2fc7157549a71ee39ec73c2bd
Guille <guillermochalliol@gmail.com>
2026-03-31 19:53:37 +0000

    v1.1.1 — add plugin-update-checker vendor library

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your-username/wp-site-monitor',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '518fa428efa80c32f8172b676b0e791cf0665355',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v5.6',
+            'version' => '5.6.0.0',
+            'reference' => 'a2db6871deec989a74e1f90fafc6d58ae526a879',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'your-username/wp-site-monitor' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '518fa428efa80c32f8172b676b0e791cf0665355',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
